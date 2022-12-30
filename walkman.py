f68f6d69ef07cd6fc96b7c6a630f6f812e8bd637
veebch <mart@veeb.ch>
2022-12-30 11:29:50 +0100

    switched to plex.tv credentials

--- walkman.py
+++ walkman.py
@@ -14,52 +14,58 @@ musicdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'music')
 configfile = os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.yaml')
 with open(configfile) as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
-baseurl=config['server']
-token=config['token']
-pathprefix=config['pathprefix']
-plex = PlexServer(baseurl, token)
-
+baseurl = config['server']
+password = config['password']
+username =config['username']
+pathprefix = config['devicepathprefix']
+account = MyPlexAccount(username, password)
+plex = account.resource(baseurl).connect()  # returns a PlexServer instance
+print('Connected to Plex')
 # download playlist tracks to current folder. 
-
+ignoredlists = ['All Music','Recently Added', 'Recently Played','Fresh ❤️', '❤️ Tracks']
 for playlist in plex.playlists(playlistType='audio'): #only output audio playlists
-    tracks = playlist.items()
-    playlist_title=playlist.title
-    print(' Playlist generation done')
-    filenamepls = os.path.join(os.path.join(musicdir,playlist_title),playlist_title+'.pls')
-    filenamem3u = os.path.join(os.path.join(musicdir,playlist_title),playlist_title+'.m3u')
-    dirname = os.path.dirname(filenamepls)
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
-    pls =open( filenamepls, 'w')
-    m3u =open( filenamem3u, 'w')
-    m3u.write('#EXTM3U\r\n')
-    m3u.write('#PLAYLIST:%s\r\n' % playlist_title)
-    m3u.write('\r\n')
-    for track in range(len(tracks)): #loop over tracks
-        media = tracks[track].media[0]
-        seconds = int(tracks[track].duration / 1000)
-        title = tracks[track].title        
-        album = tracks[track].parentTitle
-        artist = tracks[track].originalTitle
-        albumArtist = tracks[track].grandparentTitle
-        if artist == None:
-            artist = albumArtist        
-        p = Path(tracks[track].locations[0]) #get the path
-        fullpathoftrack=pathprefix+playlist.title+"/"+p.name
-        pathoftrack=playlist.title+"/"+p.name
-        m3u.write('#EXTALB:%s\n' % album)
-        m3u.write('#EXTART:%s\n' % albumArtist)
-        parts = media.parts
-        for part in parts:
-            m3u.write('#EXTINF:%s,%s - %s\r\n' % (seconds, artist, title))
-            m3u.write('%s\r\n' % fullpathoftrack)
-            m3u.write('\r\n')
-        pls.write(fullpathoftrack+'\r\n')                                      # Note the use of a Windows newline
-        path = Path("music/"+pathoftrack)                                      # It will save each playlist in its own folder
-        if path.is_file(): #skips files that already exist
-            print(f'File {path} exists - skipping')
-        else:
-            print(f'Creating {path}')
-            tracks[track].download(keep_original_name=True,savepath="music/"+playlist_title)
-    pls.close()
-    m3u.close()
+    playlist_title = playlist.title
+    if playlist_title in ignoredlists:
+        print ('Skipping Playlist: ', playlist_title)
+    else:
+        print('Processing: ',playlist_title)
+        tracks = playlist.items()
+        filenamepls = os.path.join(os.path.join(musicdir,playlist_title),playlist_title+'.pls')
+        filenamem3u = os.path.join(os.path.join(musicdir,playlist_title),playlist_title+'.m3u')
+        dirname = os.path.dirname(filenamepls)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        pls = open(filenamepls, 'w')
+        m3u = open(filenamem3u, 'w')
+        m3u.write('#EXTM3U\r\n')
+        m3u.write('#PLAYLIST:%s\r\n' % playlist_title)
+        m3u.write('\r\n')
+        for track in range(len(tracks)): #loop over tracks
+            media = tracks[track].media[0]
+            seconds = int(tracks[track].duration / 1000)
+            title = tracks[track].title        
+            print ('Title: ', title)
+            album = tracks[track].parentTitle
+            artist = tracks[track].originalTitle
+            albumArtist = tracks[track].grandparentTitle
+            if artist == None:
+                artist = albumArtist        
+            p = Path(tracks[track].locations[0]) #get the path
+            fullpathoftrack=pathprefix+playlist.title+"/"+p.name
+            pathoftrack=playlist.title+"/"+p.name
+            m3u.write('#EXTALB:%s\n' % album)
+            m3u.write('#EXTART:%s\n' % albumArtist)
+            parts = media.parts
+            for part in parts:
+                m3u.write('#EXTINF:%s,%s - %s\r\n' % (seconds, artist, title))
+                m3u.write('%s\r\n' % fullpathoftrack)
+                m3u.write('\r\n')
+            pls.write(fullpathoftrack+'\r\n')                                      # Note the use of a Windows newline
+            path = Path("music/"+pathoftrack)                                      # It will save each playlist in its own folder
+            if path.is_file(): #skips files that already exist
+                print(f'File {path} exists - skipping')
+            else:
+                print(f'Creating {path}')
+                tracks[track].download(keep_original_name=True,savepath="music/"+playlist_title)
+        pls.close()
+        m3u.close()

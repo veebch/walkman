ff28052e9e3efe84cbf41ee69134b39b177613ea
veebch <mart@veeb.ch>
2022-12-30 13:32:32 +0100

    rsync tweak

--- walkman.py
+++ walkman.py
@@ -22,6 +22,7 @@ baseurl = config['server']
 password = config['password']
 username =config['username']
 pathprefix = config['devicepathprefix']
+playerpathprefix = "/Music/"
 account = MyPlexAccount(username, password)
 plex = account.resource(baseurl).connect()  # returns a PlexServer instance
 print('Connected to Plex')
@@ -34,12 +35,10 @@ for playlist in plex.playlists(playlistType='audio'): #only output audio playlis
     else:
         print('Processing: ',playlist_title)
         tracks = playlist.items()
-        filenamepls = os.path.join(os.path.join(musicdir,playlist_title),playlist_title+'.pls')
         filenamem3u = os.path.join(os.path.join(musicdir,playlist_title),playlist_title+'.m3u')
-        dirname = os.path.dirname(filenamepls)
+        dirname = os.path.dirname(filenamem3u)
         if not os.path.exists(dirname):
             os.makedirs(dirname)
-        pls = open(filenamepls, 'w')
         m3u = open(filenamem3u, 'w')
         m3u.write('#EXTM3U\r\n')
         m3u.write('#PLAYLIST:%s\r\n' % playlist_title)
@@ -55,31 +54,29 @@ for playlist in plex.playlists(playlistType='audio'): #only output audio playlis
             if artist == None:
                 artist = albumArtist        
             p = Path(tracks[track].locations[0]) #get the path
-            fullpathoftrack=pathprefix+playlist.title+"/"+p.name
+            fullpathoftrack = playerpathprefix+playlist.title+"/"+p.name
             pathoftrack=playlist.title+"/"+p.name
             m3u.write('#EXTALB:%s\n' % album)
             m3u.write('#EXTART:%s\n' % albumArtist)
             parts = media.parts
             for part in parts:
                 m3u.write('#EXTINF:%s,%s - %s\r\n' % (seconds, artist, title))
-                m3u.write('%s\r\n' % fullpathoftrack)
+                m3u.write('\'%s\'\r\n' % fullpathoftrack)
                 m3u.write('\r\n')
-            pls.write(fullpathoftrack+'\r\n')                                      # Note the use of a Windows newline
             path = Path("music/"+pathoftrack)                                      # It will save each playlist in its own folder
             if path.is_file(): #skips files that already exist
                 print(f'File {path} exists - skipping')
             else:
                 print(f'Creating {path}')
                 tracks[track].download(keep_original_name=True,savepath="music/"+playlist_title)
-        pls.close()
         m3u.close()
-print('Using rsync on ./music and', pathprefix)
+print('Attempting rsync on ./music and', pathprefix)
 path=os.path.join('/',pathprefix)
 pluggedin = Path(path).is_dir()
-print(pluggedin)
 if pluggedin:
-    subprocess.call(["rsync", "-av", "./music/", pathprefix])
+    subprocess.call(["rsync", "-avv", "./music/", pathprefix])
 else:
-    print('Check the music player connected and that the mount point is listed in config.yaml')
+    print('Check the music player is connected and that the mount point is listed in config.yaml')
+    print('Copies of music are in the directory ./music')
 
     
